46adc4e817bcac07fcf4c971d71f782f53c3b496
Karma <mariogallegocano@gmail.com>
2020-12-08 13:36:00 +0100

    Update CACHEsym.c
    Tratamiento de direcciones finalizado. Se leen las direcciones una a una separando los datos con máscaras. El bucle finaliza con el fin del fichero. Se imprimen los datos correspondientes al número de direcciones leídas, tiempo tardado, fallos...

--- CACHEsym.c
+++ CACHEsym.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 // Mascaras de Bits
 #define BLOQ_C 0b1111111111111 // Bitmask para el Bloque
@@ -21,8 +22,9 @@ typedef struct {
 PROCESO proceso;
 
 
-//borrar int repet, solo para pruebas
-int repet = 0;
+//numero de direcciones y caracteres leidos
+float Naddr = 0;
+char CharMemoria[100];
 
 void cacheDefault(){
     const short int ETQ_Default = 255;
@@ -44,109 +46,128 @@ void cacheDefault(){
     printf("\n");
 }
 
-void actualizarLinea(){
+void printCache(){
     
+    for(int i=0; i<4; i++){
+        
+        printf("\nETQ:%02x    Datos ", proceso.t_linea_cache[i].ETQ);
+        
+        for(int j=0; j<8; j++){
+            printf("%02x ", proceso.t_linea_cache[i].Datos[j]);
+            CharMemoria[j+(i*8)] = proceso.t_linea_cache[i].Datos[j];
+        }
+    }
+    printf("\n");
     
 }
 
-int charToInt(char dirChar[]){
-    int dir = 0;
+void comparator(FILE* f_memoria, char* RAM){
     
+    int addr=0,bloq_addr=0,etq_addr=0,linea_addr=0,palabra_addr=0;
     
-    
-    return dir;
-}
-
-
-void comparator(FILE* f_memoria, char* RAM, int linea_act){
-    int dir=0,bloq_dir=0,etq_dir=0,linea_dir=0,palabra_dir=0;
-
-    // Recogemos la primera linea de accesos_memoria.txt
-    
-    // TODO : ¡SOLO ES DE UNA LINEA! Hacerlo para todas las lineas
-    
-    //borrar
-    repet++;
-    
-    fscanf(f_memoria,"%x",&dir);
-    
-    //dir = 0x02F4;
-    
-    //if(feof(f_memoria)||dir ==0){printf("\nFIN");exit(-1);}
-    // Separamos los valores de la dir mediante un bitmask
-    
-    bloq_dir= (dir >>3) & BLOQ_C;
-    etq_dir= (dir >> 5) & ETQ_C;
-    linea_dir= (dir >> 3) & LIN_C;
-    palabra_dir= dir & PALABRA_C;
-    
-    // PRINTS TEMPORALES
-    printf("\nDireccion de linea: %x",dir);
-    printf("\nRecoger Bloque: %x",bloq_dir);
-    printf("\nRecoger ETQ: %x",etq_dir);
-    printf("\nRecoger Linea: %x",linea_dir);
-    printf("\nRecoger Palabra: %x",palabra_dir);
-    printf("\n");
-    // PRINTS TEMPORALES
-
-    
-    int direccion[16];
-    
-    for(int i = 0; i<4; i++){
+    //Recogemos la siguiente línea linea de accesos_memoria.txt y la guardaos en addr
+    fscanf(f_memoria,"%x",&addr);
         
-        direccion[i] = 0;
+    //Comprobamos que no es el final del fichero, si lo es terminamos la función
+    if(feof(f_memoria)){
         
-    }
-    printf("\n");
+        printf("\n-FIN DE DIRECCIONES DE MEMORIA-\n");
+        
+    }else{
+        
+        //incrementamos el número de addrecciones leídas
+        Naddr++;
 
-    if(etq_dir == proceso.t_linea_cache[linea_act].ETQ){
-        printf("\nSon iguales");
-    }
-    else{
-        proceso.numfallos++;
-        // TODO : Colocar bien el valor de ADDR
-        printf("\nT: %d, Fallo de CACHE %d, ADDR %04x ETQ %x linea %02x palabra %02x bloque %02x",proceso.tiempoglobal,proceso.numfallos,0,etq_dir,linea_dir,palabra_dir,bloq_dir);
-        proceso.tiempoglobal += 10;
+        // Separamos los valores de la addr mediante un bitmask
+        bloq_addr= (addr >>3) & BLOQ_C;
+        etq_addr= (addr >> 5) & ETQ_C;
+        linea_addr= (addr >> 3) & LIN_C;
+        palabra_addr= addr & PALABRA_C;
         
-        // TODO : Crear funcion
-        printf("\nCargando el bloque %02d en la linea %02x",bloq_dir,linea_dir);
-        printf("\nT: %d, Acierto de CACHE, ADDR %04x ETQ %x linea %02x palabra %02x DATO %02x",proceso.tiempoglobal,0,etq_dir,linea_dir,palabra_dir,RAM[bloq_dir]);
-        proceso.t_linea_cache[linea_act].ETQ = etq_dir;
-        
-        //asignamos uno a uno los valores correspondientes a los datos de la etiqueta que hemos leido
-        //para ello, buscamos en el array RAM el bloque dedirección + i
-        //ya que hemos de buscar 8 datos
-        
-
-        for(int i = 0; i<8;i++){
-            
-            proceso.t_linea_cache[linea_act].Datos[i] = RAM[(bloq_dir+i)];
-            
-        }
-        
-        for(int i=0; i<4; i++){
-            
-            printf("\nETQ:%x    Datos ", proceso.t_linea_cache[i].ETQ);
-            
-            for(int j=0; j<8; j++){
-                printf("%x ", proceso.t_linea_cache[i].Datos[j]);
-            }
-        }
+        //Mostramos los datos de la dirección
+        printf("\naddreccion de linea: %x",addr);
+        printf("\nRecoger Bloque: %x",bloq_addr);
+        printf("\nRecoger ETQ: %x",etq_addr);
+        printf("\nRecoger Linea: %x",linea_addr);
+        printf("\nRecoger Palabra: %x",palabra_addr);
         printf("\n");
+
+        //Comprobamos si la etiqueta de la addrección está cargada en caché en la linea
+        if(etq_addr == proceso.t_linea_cache[linea_addr].ETQ){
+            
+            // si es así, mostramos un acierto y la caché cargada
+            
+            printf("\nT: %d, Acierto de CACHE, ADDR %04x ETQ %x linea %02x palabra %02x DATO %02x",proceso.tiempoglobal,addr,etq_addr,linea_addr,palabra_addr,RAM[addr]);
+            printf("\n");
+
+            printCache();
+
+        }
+        else{
+            
+            //de lo contrario, incrementamos el número de fallos
+            proceso.numfallos++;
+
+            //mostramos el tiempo, número de fallos y datos de la addrección
+            printf("\nT: %d, Fallo de CACHE %d, ADDR %04x ETQ %x linea %02x palabra %02x bloque %02x",proceso.tiempoglobal,proceso.numfallos,addr,etq_addr,linea_addr,palabra_addr,bloq_addr);
+            
+            //e incrementamos en 10 el tiempo global
+            proceso.tiempoglobal += 10;
+            
+            //cargamos la addrección en su correspondiente línea de caché
+            printf("\nCargando el bloque %02x en la linea %02x",bloq_addr,linea_addr);
+            printf("\nT: %d, Acierto de CACHE, ADDR %04x ETQ %x linea %02x palabra %02x DATO %02x",proceso.tiempoglobal,addr,etq_addr,linea_addr,palabra_addr,RAM[addr]);
+            
+            proceso.t_linea_cache[linea_addr].ETQ = etq_addr;
+            
+            //y cargamos en caché el bloque de la addrección en su correspondiene línea
+            //para ello, buscamos en el array RAM el bloque deaddrección + i
+            //ya que hemos de buscar 8 datos
+            for(int i = 0; i<8;i++){
+                
+                proceso.t_linea_cache[linea_addr].Datos[i] = RAM[((addr-palabra_addr)+7-i)];
+                
+            }
+            
+            printf("\n");
+
+            //mostramos la caché actual
+            
+            printCache();
+            
+        }
         
+        //hacemos un sleep de 2 segundos y lo incrementamos en el tiempo global
+        sleep(1);
+        proceso.tiempoglobal += 2;
         
-        actualizarLinea();
+        //llamamos de nuevo a esta misma función para vargar la siguiente addrección
+        comparator(f_memoria, RAM);
+        
     }
-    if(linea_act<4){linea_act++;}
-    
-    //borrar
-    if(repet <2){
-        comparator(f_memoria, RAM,linea_act);
-}
-
     
 }
 
+void dataPrint(){
+    
+    printf("Se han leído un total de %.0f direcciones de memoria.\nHay un total de %d fallos, dando una media de %.2f fallos por dirección.\nSe ha tardado un total de %d segundos a una media de %.2f segundos por cada dirección.\n",Naddr,proceso.numfallos,(proceso.numfallos/Naddr),proceso.tiempoglobal,(proceso.tiempoglobal/Naddr));
+    
+    
+    //mostramos todos los caracteres guardados en caché
+    
+    printf("\nValores guardados en caché:\n");
+    int salt = 0;
+
+    for(int i = 0; i < 32; i++){
+        
+        if(salt == 8){salt = 0; printf("\n");}
+        printf("%c ",CharMemoria[i]);
+        salt++;
+        
+    }
+    printf("\n\n");
+ 
+}
 
 int main(){
     int i=0;
@@ -161,8 +182,7 @@ int main(){
     
     // Lectura de fichero RAM.bin
     
-    //f_ram = fopen("RAM.bin","r");
-    f_ram = fopen("/Users/mario/Desktop/U-TAD/proyectos Xcode/C/proyecto_ssoo_2/proyecto_ssoo_2/RAM.bin","r");
+    f_ram = fopen("RAM.bin","r");
     
     if(f_ram == NULL){
         printf("El archivo RAM.bin no se ha podido abrir\n");
@@ -178,20 +198,22 @@ int main(){
     
     // Lectura de fichero accesos_memoria.txt
     
-    //f_ram = fopen("accesos_memoria.txt","r");
-    f_memoria = fopen("/Users/mario/Desktop/U-TAD/proyectos Xcode/C/proyecto_ssoo_2/proyecto_ssoo_2/accesos_memoria.txt","r");
+    f_ram = fopen("accesos_memoria.txt","r");
     
     if(f_memoria == NULL){
         printf("El archivo accesos_memoria.txt no se ha podido abrir\n");
         exit(-1);
     }
 
-    // Circuito comparador de la correspondencia directa
+    // Circuito comparador de la correspondencia addrecta
+    comparator(f_memoria, RAM);
+    printf("\n");
     
-    comparator(f_memoria, RAM,0);
-    
+    //Cerramos lo ficheros
     fclose(f_ram);
     fclose(f_memoria);
+        
+    dataPrint();
     
     return 0;
 }
